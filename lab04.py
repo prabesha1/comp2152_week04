3086f13c75b9a435b1436ab0a26ce07eccfaed13
prabesha1 <prabesha1@outlook.com>
2025-01-28 11:47:59 -0500

    Question 7

--- lab04.py
+++ lab04.py
@@ -171,6 +171,11 @@ loot  = loot_options.pop(lootRoll - 1)
 belt.append(loot)
 print("Your belt: ", belt)
 
+#Lab04 Q7 - Sort the belt
+print("You're neat, so organize your belt alphabetically:")
+belt.sort()
+print("your belt: ", belt)
+
 input("Analyze the roll (Press enter)")
 # Compare Player vs Monster's strength
 print("--- You are matched in strength: " + str(combat_strength == m_combat_strength))
